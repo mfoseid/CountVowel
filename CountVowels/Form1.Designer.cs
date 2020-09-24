5887baf1aa03b384e9a80bf73b7aafaf9c034052
michael <michael.foseid@my.sctcc.edu>
2020-09-23 23:51:14 -0500

    adding program to master...i think

---
+++ CountVowels/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace CountVowels
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TxtInput = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.BtnCount = new System.Windows.Forms.Button();
+            this.LblResult = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // TxtInput
+            // 
+            this.TxtInput.Location = new System.Drawing.Point(21, 208);
+            this.TxtInput.MinimumSize = new System.Drawing.Size(250, 200);
+            this.TxtInput.Multiline = true;
+            this.TxtInput.Name = "TxtInput";
+            this.TxtInput.Size = new System.Drawing.Size(250, 200);
+            this.TxtInput.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(243, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Please enter a phrase or paragraph. ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(18, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(241, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "All vowels within said body of text will ";
+            // 
+            // BtnCount
+            // 
+            this.BtnCount.Location = new System.Drawing.Point(21, 129);
+            this.BtnCount.Name = "BtnCount";
+            this.BtnCount.Size = new System.Drawing.Size(133, 50);
+            this.BtnCount.TabIndex = 3;
+            this.BtnCount.Text = "Count";
+            this.BtnCount.UseVisualStyleBackColor = true;
+            this.BtnCount.Click += new System.EventHandler(this.BtnCount_Click);
+            // 
+            // LblResult
+            // 
+            this.LblResult.AutoSize = true;
+            this.LblResult.Location = new System.Drawing.Point(187, 146);
+            this.LblResult.Name = "LblResult";
+            this.LblResult.Size = new System.Drawing.Size(16, 17);
+            this.LblResult.TabIndex = 4;
+            this.LblResult.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(18, 71);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(185, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "be counted. Y is not a vowel";
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(360, 439);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.LblResult);
+            this.Controls.Add(this.BtnCount);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TxtInput);
+            this.MinimumSize = new System.Drawing.Size(378, 400);
+            this.Name = "FrmMain";
+            this.Text = "Count Vowel";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TxtInput;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button BtnCount;
+        private System.Windows.Forms.Label LblResult;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
